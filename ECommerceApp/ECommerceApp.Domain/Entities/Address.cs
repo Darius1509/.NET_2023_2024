41847b33cd25e684f2beaa76a7e529cfdecb1741
miruna-iosub <mirunaelenaiii2403@gmail.com>
2023-11-05 15:28:53 +0200

    Added two more entity classes

---
+++ ECommerceApp/ECommerceApp.Domain/Entities/Address.cs
@@ -0,0 +1,49 @@
+﻿using ECommerceApp.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApp.Domain.Entities
+{
+    public class Address
+    {
+        public Address(string streetName, int postalCode, string city, string country)
+        {
+            AddressId = Guid.NewGuid();
+            StreetName = streetName;
+            PostalCode = postalCode;
+            City = city;
+            Country = country;
+        }
+
+        public Guid AddressId { get; private set; }
+        public string StreetName { get; private set; }
+        public int PostalCode { get; private set; }
+        public string City { get; private set; }
+        public string Country { get; private set; }
+
+        public static Result<Address> Create(string streetName, int postalCode, string city, string country)
+        {
+            if (string.IsNullOrEmpty(streetName))
+            {
+                return Result<Address>.Failure("Street name cannot be empty");
+            }
+            if (postalCode <= 0)
+            {
+                return Result<Address>.Failure("Postal code cannot be less than or equal to zero");
+            }
+            if (string.IsNullOrEmpty(city))
+            {
+                return Result<Address>.Failure("City cannot be empty");
+            }
+            if (string.IsNullOrEmpty(country))
+            {
+                return Result<Address>.Failure("Country cannot be empty");
+            }
+
+            return Result<Address>.Success(new Address(streetName, postalCode, city, country));
+        }
+    }
+}
---
+++ ECommerceApp/ECommerceApp.Domain/Entities/Order.cs
@@ -0,0 +1,57 @@
+﻿using ECommerceApp.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApp.Domain.Entities
+{
+    public class Order
+    {
+        private Order(DateTime orderDate, string orderStatus, List<Product> orderProducts, Customer orderCustomer)
+        {
+            OrderId = Guid.NewGuid();
+            OrderDate = orderDate;
+            OrderStatus = orderStatus;
+            OrderProducts = orderProducts;
+            OrderCustomer = orderCustomer;
+        }
+
+        public Guid OrderId { get; private set; }
+        public DateTime OrderDate { get; private set; }
+        public string OrderStatus { get; private set; }
+        public List<Product> OrderProducts { get; private set; }
+        public Customer OrderCustomer { get; private set; }
+
+        public static Result<Order> Create(DateTime orderDate, string orderStatus, Customer orderCustomer)
+        {
+            if (orderDate == DateTime.MinValue)
+            {
+                return Result<Order>.Failure("Order date cannot be empty");
+            }
+            if (string.IsNullOrEmpty(orderStatus))
+            {
+                return Result<Order>.Failure("Order status cannot be empty");
+            }
+            if (orderCustomer == null)
+            {
+                return Result<Order>.Failure("Order customer cannot be null");
+            }
+
+            List<Product> orderProducts = new List<Product>();
+
+            return Result<Order>.Success(new Order(orderDate, orderStatus, orderProducts, orderCustomer));
+        }
+
+        public void AttachProduct(Product product)
+        {
+            OrderProducts.Add(product);
+        }
+
+        public void UpdateStatus(string status)
+        {
+            OrderStatus = status;
+        }
+    }
+}
