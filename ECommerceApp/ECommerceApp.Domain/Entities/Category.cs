6364b3353391701ba8e5055cac2c2eaac3a67196
mateiPruteanu <mateipruteanu1@gmail.com>
2023-11-06 13:34:59 +0200

    Added Category Entity

--- ECommerceApp/ECommerceApp.Domain/Entities/Category.cs
+++ ECommerceApp/ECommerceApp.Domain/Entities/Category.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ECommerceApp.Domain.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,30 @@ namespace ECommerceApp.Domain.Entities
 {
     public class Category
     {
+        private Category(Guid categoryId, string categoryName)
+        {
+            CategoryId = categoryId;
+            CategoryName = categoryName;
+        }
+
+        public Guid CategoryId { get; private set; }
+        public string CategoryName { get; private set; }
+        public List<Product>? Products { get; private set; }
+
+        public static Result<Category> Create(string categoryName)
+        {
+            if (string.IsNullOrEmpty(categoryName))
+            {
+                return Result<Category>.Failure("Category name cannot be empty");
+            }
+
+            return Result<Category>.Success(new Category(Guid.NewGuid(), categoryName));
+        }
+
+        public void AttachProduct(Product product)
+        {
+            if (Products == null) { Products = new List<Product>(); }
+            Products.Add(product);
+        }
     }
 }
