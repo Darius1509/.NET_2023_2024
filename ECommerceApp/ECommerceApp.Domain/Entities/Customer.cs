ea4b9e28381cf2d0d950d092ab052a57ca80c170
Sabjen79 <florinbalan7979@gmail.com>
2023-11-06 12:53:42 +0200

    Added Customer Entity

--- ECommerceApp/ECommerceApp.Domain/Entities/Customer.cs
+++ ECommerceApp/ECommerceApp.Domain/Entities/Customer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ECommerceApp.Domain.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,58 @@ namespace ECommerceApp.Domain.Entities
 {
     public class Customer
     {
+        private Customer(String name, String email, String pass)
+        {
+            CustomerId = Guid.NewGuid();
+            CustomerName = name;
+            CustomerEmail = email;
+            CustomerPassword = pass;
+        }
+
+        public Guid CustomerId { get; private set; }
+        public String CustomerName { get; private set; }
+        public String CustomerEmail { get; private set; }
+        public String CustomerPassword { get; private set; }
+        public List<Address>? CustomerAddresses { get; private set; }
+        public List<Product>? CustomerWishlist { get; private set; }
+        public List<Order>? CustomerOrders { get; private set; }
+
+        public static Result<Customer> Create(String name, String email, String pass)
+        {
+            if (String.IsNullOrWhiteSpace(name))
+            {
+                return Result<Customer>.Failure("Customer name cannot be empty.");
+            }
+
+            if (String.IsNullOrWhiteSpace(email))
+            {
+                return Result<Customer>.Failure("Customer email cannot be empty.");
+            }
+
+            if (String.IsNullOrWhiteSpace(pass))
+            {
+                return Result<Customer>.Failure("Customer password cannot be empty.");
+            }
+
+            return Result<Customer>.Success(new Customer(name, email, pass));
+        }
+
+        public void AttachAddress(Address address)
+        {
+            if (CustomerAddresses == null) { CustomerAddresses = new List<Address>(); }
+            CustomerAddresses.Add(address);
+        }
+
+        public void AttachProduct(Product product)
+        {
+            if (CustomerWishlist == null) { CustomerWishlist = new List<Product>(); }
+            CustomerWishlist.Add(product);
+        }
+
+        public void AttachOrder(Order order)
+        {
+            if (CustomerOrders == null) { CustomerOrders = new List<Order>(); }
+            CustomerOrders.Add(order);
+        }
     }
 }
