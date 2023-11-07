48845be8518ca8f0657129bafe53dbab8d187759
Sabjen79 <florinbalan7979@gmail.com>
2023-11-07 15:01:55 +0200

    Added CustomerRepository and its Interface

---
+++ ECommerceApp/Infrastructure/Repositories/CustomerRepository.cs
@@ -0,0 +1,17 @@
+﻿using ECommerceApp.Application.Contracts;
+using ECommerceApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    internal class CustomerRepository : BaseRepository<Customer>, ICustomerRepository
+    {
+        public CustomerRepository(ECommerceAppContext context) : base(context)
+        {
+        }
+    }
+}
